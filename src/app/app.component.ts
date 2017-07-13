556616c14d35e1dc3a66a484d175eae66ce77851
Ashish Surana <ashishsurana12345@gmail.com>
2017-07-13 07:42:46 +0530

    UI Form completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+    addSupplierForm: FormGroup;
+  name = new FormControl('', Validators.required);
+  address = new FormControl('', Validators.required);
+  phone = new FormControl('', Validators.required);
+  email = new FormControl('', Validators.required);
+  constructor(    private formBuilder: FormBuilder
+) {
+this.addSupplierForm = this.formBuilder.group({
+      name: this.name,
+      address: this.address,
+      phone: this.phone,
+      email: this.email
+    });
+  }
+
+  submitInfo() {
+    console.log('Submit', this.addSupplierForm.value);
+  }
 }
